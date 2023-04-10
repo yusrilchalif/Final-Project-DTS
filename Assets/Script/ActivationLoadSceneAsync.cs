a63b42e20eb59810b3847d2035e209cb4beddf8b
Yusril <yusrilchalif@gmail.com>
2023-04-10 13:45:53 +0700

    init

---
+++ Assets/Script/ActivationLoadSceneAsync.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ActivationLoadSceneAsync : MonoBehaviour
+{
+    [Header("Loading Screen")]
+    public LoadSceneMode LoadingType;
+    public string LoadingScene;
+    string TargetScene;
+
+    [Header("Event Settings")]
+    public UnityEvent StartEvents;
+    public UnityEvent UpdateEvents;
+
+    [Header("Delay Settings")]
+    public bool usingDelay;
+    public float Delay;
+    public UnityEvent DelayEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartEvents?.Invoke();
+        if (usingDelay)
+        {
+            Invoke("LoadScene", Delay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEvents?.Invoke();
+    }
+
+    void LoadScene()
+    {
+        DelayEvents?.Invoke();
+    }
+
+    public void LoadScene(string aValue)
+    {
+        //Melakukan perpindahan antar scene. Catatan: Scene yang dipanggil sudah didaftarkan di Build Setting
+        TargetScene = aValue;
+        StartCoroutine(LoadAsyncScene());
+    }
+
+    IEnumerator LoadAsyncScene()
+    {
+        SceneManager.LoadSceneAsync(LoadingScene, LoadSceneMode.Additive);
+        yield return new WaitForSeconds(1);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(TargetScene, LoadingType);
+
+        while (!asyncLoad.isDone)
+        {
+            PlayerPrefs.SetFloat("Loading", asyncLoad.progress);
+            yield return null;
+        }
+
+        SceneManager.UnloadSceneAsync(LoadingScene);
+    }
+
+}
---
+++ Assets/Script/LoadSceneDelay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneDelay : MonoBehaviour
+{
+
+    [Header("Main Settings")]
+    public string TargetScene;
+    public float Delay;
+
+    void LoadScene()
+    {
+        //Melakukan perpindahan antar scene. Catatan: Scene yang dipanggil sudah didaftarkan di Build Setting
+        SceneManager.LoadScene(TargetScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadScene", Delay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
